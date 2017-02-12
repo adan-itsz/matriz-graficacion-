a7d92ae87a26274dc10c5879c0ce820db4169c0f
Adan De Santiago Avalos <adan1995a@gmail.com>
2017-02-12 17:55:28 -0600

    coloca random en matriz

--- Graficacion.js
+++ Graficacion.js
@@ -2,6 +2,16 @@ var ancho = prompt("ancho de la matriz");
 var alto1 = prompt("alto de la matriz");
 var celda;
 var textoCelda;
+var matriz= new Array(alto1);
+
+for(var i=0;i<alto1;i++){   //creacion de matriz
+  matriz[i]=new Array(ancho);
+}
+for(var i=0;i<alto1;i++){   //inicializar matriz
+  for(var j=0;j<ancho;j++){
+    matriz[i][j]=0;
+  }
+}
 
 //se obtiene el body
 var body = document.getElementsByTagName("body")[0];
@@ -10,33 +20,27 @@ var body = document.getElementsByTagName("body")[0];
 var tabla = document.createElement("table");
 //se le pone borde a la tabla
 tabla.style.border = "0px solid #000"
-
 var tblBody = document.createElement("tbody");
 
-
-
 //Crear las celdas----------------------
-for(var i=0;i<alto1;i++){
+for(var i=0;i<matriz.length;i++){
   //se crean el alto
-var alto = tblBody.insertRow(i);
+var alto = tblBody.insertRow(i);//fila
     for (var j= 0; j < ancho;j++) {
     //Crear un elemento <td> osea el ancho
     // un nodo de texto
-
-    celda= alto.insertCell(j);
+    celda= alto.insertCell(j);//columna
     celda.height="20px";
     celda.width="50px";
     //se le da un id a cada celda
     celda.id=i+":"+j;
     //se agrega evento listener a las celdas----------falta revisar!---------
-    celda.addEventListener("click",function myfunc(){celda.style.backgroundColor="red";});
 //se le agrega los ceros a cada celda
-      textoCelda = document.createTextNode("0");
+      textoCelda = document.createTextNode(matriz[i][j]);
       //se le pone borde a cada celda
       celda.style.border = "1px solid #000"
       //se pinta cada celda
       celda.style.backgroundColor = "#E9E3E9";
-
       celda.appendChild(textoCelda);
       alto.appendChild(celda);
     }
@@ -47,19 +51,16 @@ var alto = tblBody.insertRow(i);
 //posiciona el <tbody> debajo del elemento <table>
 tabla.appendChild(tblBody);
 body.appendChild(tabla);
+var randomFila= Math.round(Math.random()*alto1);
+var randomColumna=Math.round(Math.random()*ancho);
+var cell=document.getElementById(randomFila+ ":"+randomColumna);
 
+cell.style.backgroundColor='red';
 
-$(document).ready(function(){
-        $(".boton").click(function(){
+//var index=tblBody.fil[randomFila].col[randomColumna].childNodes[0].value;
+window.alert(randomFila + " " +randomColumna);
+//index.style.backgroundColor="red";
 
-            var valores="";
-
-            // Obtenemos todos los valores contenidos en los <td> de la fila
-            // seleccionada
-            $(this).parents("tr").find("td").each(function(){
-                valores+=$(this).html()+"\n";
-            });
-
-            alert(valores);
-        });
-    });
+//function pintar(filaa,columnaa){
+tblBody.addEventListener("click",function myfunc(){col.style.backgroundColor="red";});
+//}
